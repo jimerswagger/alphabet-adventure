0f725d6ee1f2afc9ab0853efbc98c548238ae214
jimerswagger <jimmysaldana163@gmail.com>
2024-12-26 17:15:17 -0500

    Left and Right Anima & Idle
    Changed AnimatedCharacter2D node animations to just having a Sprite 2D node and an animationplayer node. Now character can move left and right with the right animation in the right direction and the player has an idle animation when not moving.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 
+@onready var _animation_player = $AnimationPlayer
+
 
 const SPEED = 250.0
 const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta):
+	#_animated_sprite.play("idle")
+	#_animation_player.play("idle")
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -18,8 +22,12 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
+		#_animated_sprite.play("run") #used for when using animations with animatedsprite2d
+		$Sprite2D.flip_h = int(direction-1)
+		_animation_player.play("run")
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		_animation_player.play("idle")
 
 	move_and_slide()
