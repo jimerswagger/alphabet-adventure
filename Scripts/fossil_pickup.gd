02b529bf40c4aadccb9f5f5e2ea149989543529a
jimerswagger <jimmysaldana163@gmail.com>
2025-01-03 20:08:23 -0500

    added sound for fossil pickup(YATTA)

--- Scripts/fossil_pickup.gd
+++ Scripts/fossil_pickup.gd
@@ -3,5 +3,10 @@ extends Area2D
 
 
 
-func _on_body_entered(body):
-	queue_free()
+func _on_body_entered(_body):
+	$YATTA.play()
+	
+
+
+func _on_yatta_finished():
+	queue_free() # Replace with function body.
